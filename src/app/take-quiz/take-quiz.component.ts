e640181e2f3736b6aeb88a089093a28af2190b9e
Damniel009 <vargadaniel28@users.noreply.github.com>
2021-04-02 16:08:21 +0300

    Progressed with take-quiz component. There is no interaction with the quiz yet, but questions are displayed correctly

--- src/app/take-quiz/take-quiz.component.ts
+++ src/app/take-quiz/take-quiz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { QuizService } from '../services/quiz.service';
 
 @Component({
   selector: 'app-take-quiz',
@@ -8,10 +9,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TakeQuizComponent implements OnInit {
   quizId: string = this.route.snapshot.paramMap.get('id');
+  quiz;
+  radios
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private quizService: QuizService
+  ) {}
 
   ngOnInit(): void {
-    console.log(this.quizId);
+    this.quizService.getQuiz(this.quizId).subscribe((res) => {
+      this.quiz = res.questions;
+      console.log(this.quiz);
+    });
   }
 }
