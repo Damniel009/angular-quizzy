004764eca5197e1184965d041387fef27be71bc9
Damniel009 <vargadaniel28@users.noreply.github.com>
2021-05-29 18:24:16 +0300

    Changed site's title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit{
-  title = 'Quizzy';
+  title = 'MySeriesWatchlist';
 
   constructor(private authService: AuthService) {}
 
